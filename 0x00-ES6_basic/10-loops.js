cd94bfc61ba92bef3865c6ed37bd3627df642151
Iniubong Arthur <iarthur@aortem.io>
2024-10-01 02:03:03 +0000

    task 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
+  const myArray = [];
   for (let idx of array) {
     idx = appendString + idx;
+    myArray.push(idx);
   }
-
-  return array;
+  return myArray;
 }
